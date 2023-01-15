c331bf6dec552f179a8c7bfb855e861ed16fadff
wagiminator <wagiminator@web.de>
2023-01-15 21:22:45 +0100

    Added new board

--- CH55xE_DevStick/software/blink/include/gpio.h
+++ CH55xE_DevStick/software/blink/include/gpio.h
@@ -18,6 +18,9 @@
 // PIN_read(PIN)            Read PIN input value
 // PIN_write(PIN, val)      Write PIN output value (0 = LOW / 1 = HIGH)
 //
+// PIN_asm(PIN)             Convert PIN for inline assembly: setb PIN_asm(PIN_LED)
+// PIN_set(PIN)             Convert PIN for direct manipulation: PIN_set(PIN_LED) = 1;
+//
 // PWM_start(PIN)           Start PWM output on PIN (must be a PWM-capable pin!!!)
 // PWM_stop(PIN)            Stop PWM output on PIN
 // PWM_write(PIN, val)      Set PWM output active level duty cycle on PIN
@@ -39,22 +42,22 @@
 enum{P10, P11, P12, P13, P14, P15, P16, P17, P30, P31, P32, P33, P34, P35, P36, P37};
 
 // Define pins for direct bit manipulation (these are for internal use only)
-SBIT(P1_0, 0x90, 0);
-SBIT(P1_1, 0x90, 1);
-SBIT(P1_2, 0x90, 2);
-SBIT(P1_3, 0x90, 3);
-SBIT(P1_4, 0x90, 4);
-SBIT(P1_5, 0x90, 5);
-SBIT(P1_6, 0x90, 6);
-SBIT(P1_7, 0x90, 7);
-SBIT(P3_0, 0xB0, 0);
-SBIT(P3_1, 0xB0, 1);
-SBIT(P3_2, 0xB0, 2);
-SBIT(P3_3, 0xB0, 3);
-SBIT(P3_4, 0xB0, 4);
-SBIT(P3_5, 0xB0, 5);
-SBIT(P3_6, 0xB0, 6);
-SBIT(P3_7, 0xB0, 7);
+SBIT(PP10, 0x90, 0);
+SBIT(PP11, 0x90, 1);
+SBIT(PP12, 0x90, 2);
+SBIT(PP13, 0x90, 3);
+SBIT(PP14, 0x90, 4);
+SBIT(PP15, 0x90, 5);
+SBIT(PP16, 0x90, 6);
+SBIT(PP17, 0x90, 7);
+SBIT(PP30, 0xB0, 0);
+SBIT(PP31, 0xB0, 1);
+SBIT(PP32, 0xB0, 2);
+SBIT(PP33, 0xB0, 3);
+SBIT(PP34, 0xB0, 4);
+SBIT(PP35, 0xB0, 5);
+SBIT(PP36, 0xB0, 6);
+SBIT(PP37, 0xB0, 7);
 
 // ===================================================================================
 // Set pin as INPUT (high impedance, no pullup)
@@ -93,125 +96,145 @@ SBIT(P3_7, 0xB0, 7);
 // Set pin to LOW
 // ===================================================================================
 #define PIN_low(PIN) \
-  (PIN == P10 ? (P1_0 = 0) : \
-  (PIN == P11 ? (P1_1 = 0) : \
-  (PIN == P12 ? (P1_2 = 0) : \
-  (PIN == P13 ? (P1_3 = 0) : \
-  (PIN == P14 ? (P1_4 = 0) : \
-  (PIN == P15 ? (P1_5 = 0) : \
-  (PIN == P16 ? (P1_6 = 0) : \
-  (PIN == P17 ? (P1_7 = 0) : \
-  (PIN == P30 ? (P3_0 = 0) : \
-  (PIN == P31 ? (P3_1 = 0) : \
-  (PIN == P32 ? (P3_2 = 0) : \
-  (PIN == P33 ? (P3_3 = 0) : \
-  (PIN == P34 ? (P3_4 = 0) : \
-  (PIN == P35 ? (P3_5 = 0) : \
-  (PIN == P36 ? (P3_6 = 0) : \
-  (PIN == P37 ? (P3_7 = 0) : \
+  (PIN == P10 ? (PP10 = 0) : \
+  (PIN == P11 ? (PP11 = 0) : \
+  (PIN == P12 ? (PP12 = 0) : \
+  (PIN == P13 ? (PP13 = 0) : \
+  (PIN == P14 ? (PP14 = 0) : \
+  (PIN == P15 ? (PP15 = 0) : \
+  (PIN == P16 ? (PP16 = 0) : \
+  (PIN == P17 ? (PP17 = 0) : \
+  (PIN == P30 ? (PP30 = 0) : \
+  (PIN == P31 ? (PP31 = 0) : \
+  (PIN == P32 ? (PP32 = 0) : \
+  (PIN == P33 ? (PP33 = 0) : \
+  (PIN == P34 ? (PP34 = 0) : \
+  (PIN == P35 ? (PP35 = 0) : \
+  (PIN == P36 ? (PP36 = 0) : \
+  (PIN == P37 ? (PP37 = 0) : \
 (0)))))))))))))))))
 
 // ===================================================================================
 // Set pin to HIGH
 // ===================================================================================
 #define PIN_high(PIN) \
-  (PIN == P10 ? (P1_0 = 1) : \
-  (PIN == P11 ? (P1_1 = 1) : \
-  (PIN == P12 ? (P1_2 = 1) : \
-  (PIN == P13 ? (P1_3 = 1) : \
-  (PIN == P14 ? (P1_4 = 1) : \
-  (PIN == P15 ? (P1_5 = 1) : \
-  (PIN == P16 ? (P1_6 = 1) : \
-  (PIN == P17 ? (P1_7 = 1) : \
-  (PIN == P30 ? (P3_0 = 1) : \
-  (PIN == P31 ? (P3_1 = 1) : \
-  (PIN == P32 ? (P3_2 = 1) : \
-  (PIN == P33 ? (P3_3 = 1) : \
-  (PIN == P34 ? (P3_4 = 1) : \
-  (PIN == P35 ? (P3_5 = 1) : \
-  (PIN == P36 ? (P3_6 = 1) : \
-  (PIN == P37 ? (P3_7 = 1) : \
+  (PIN == P10 ? (PP10 = 1) : \
+  (PIN == P11 ? (PP11 = 1) : \
+  (PIN == P12 ? (PP12 = 1) : \
+  (PIN == P13 ? (PP13 = 1) : \
+  (PIN == P14 ? (PP14 = 1) : \
+  (PIN == P15 ? (PP15 = 1) : \
+  (PIN == P16 ? (PP16 = 1) : \
+  (PIN == P17 ? (PP17 = 1) : \
+  (PIN == P30 ? (PP30 = 1) : \
+  (PIN == P31 ? (PP31 = 1) : \
+  (PIN == P32 ? (PP32 = 1) : \
+  (PIN == P33 ? (PP33 = 1) : \
+  (PIN == P34 ? (PP34 = 1) : \
+  (PIN == P35 ? (PP35 = 1) : \
+  (PIN == P36 ? (PP36 = 1) : \
+  (PIN == P37 ? (PP37 = 1) : \
 (0)))))))))))))))))
 
 // ===================================================================================
 // TOGGLE pin
 // ===================================================================================
 #define PIN_toggle(PIN) \
-  (PIN == P10 ? (P1_0 = !P1_0) : \
-  (PIN == P11 ? (P1_1 = !P1_1) : \
-  (PIN == P12 ? (P1_2 = !P1_2) : \
-  (PIN == P13 ? (P1_3 = !P1_3) : \
-  (PIN == P14 ? (P1_4 = !P1_4) : \
-  (PIN == P15 ? (P1_5 = !P1_5) : \
-  (PIN == P16 ? (P1_6 = !P1_6) : \
-  (PIN == P17 ? (P1_7 = !P1_7) : \
-  (PIN == P30 ? (P3_0 = !P3_0) : \
-  (PIN == P31 ? (P3_1 = !P3_1) : \
-  (PIN == P32 ? (P3_2 = !P3_2) : \
-  (PIN == P33 ? (P3_3 = !P3_3) : \
-  (PIN == P34 ? (P3_4 = !P3_4) : \
-  (PIN == P35 ? (P3_5 = !P3_5) : \
-  (PIN == P36 ? (P3_6 = !P3_6) : \
-  (PIN == P37 ? (P3_7 = !P3_7) : \
+  (PIN == P10 ? (PP10 = !PP10) : \
+  (PIN == P11 ? (PP11 = !PP11) : \
+  (PIN == P12 ? (PP12 = !PP12) : \
+  (PIN == P13 ? (PP13 = !PP13) : \
+  (PIN == P14 ? (PP14 = !PP14) : \
+  (PIN == P15 ? (PP15 = !PP15) : \
+  (PIN == P16 ? (PP16 = !PP16) : \
+  (PIN == P17 ? (PP17 = !PP17) : \
+  (PIN == P30 ? (PP30 = !PP30) : \
+  (PIN == P31 ? (PP31 = !PP31) : \
+  (PIN == P32 ? (PP32 = !PP32) : \
+  (PIN == P33 ? (PP33 = !PP33) : \
+  (PIN == P34 ? (PP34 = !PP34) : \
+  (PIN == P35 ? (PP35 = !PP35) : \
+  (PIN == P36 ? (PP36 = !PP36) : \
+  (PIN == P37 ? (PP37 = !PP37) : \
 (0)))))))))))))))))
 
 // ===================================================================================
 // READ pin
 // ===================================================================================
 #define PIN_read(PIN) \
-  (PIN == P10 ? (P1_0) : \
-  (PIN == P11 ? (P1_1) : \
-  (PIN == P12 ? (P1_2) : \
-  (PIN == P13 ? (P1_3) : \
-  (PIN == P14 ? (P1_4) : \
-  (PIN == P15 ? (P1_5) : \
-  (PIN == P16 ? (P1_6) : \
-  (PIN == P17 ? (P1_7) : \
-  (PIN == P30 ? (P3_0) : \
-  (PIN == P31 ? (P3_1) : \
-  (PIN == P32 ? (P3_2) : \
-  (PIN == P33 ? (P3_3) : \
-  (PIN == P34 ? (P3_4) : \
-  (PIN == P35 ? (P3_5) : \
-  (PIN == P36 ? (P3_6) : \
-  (PIN == P37 ? (P3_7) : \
+  (PIN == P10 ? (PP10) : \
+  (PIN == P11 ? (PP11) : \
+  (PIN == P12 ? (PP12) : \
+  (PIN == P13 ? (PP13) : \
+  (PIN == P14 ? (PP14) : \
+  (PIN == P15 ? (PP15) : \
+  (PIN == P16 ? (PP16) : \
+  (PIN == P17 ? (PP17) : \
+  (PIN == P30 ? (PP30) : \
+  (PIN == P31 ? (PP31) : \
+  (PIN == P32 ? (PP32) : \
+  (PIN == P33 ? (PP33) : \
+  (PIN == P34 ? (PP34) : \
+  (PIN == P35 ? (PP35) : \
+  (PIN == P36 ? (PP36) : \
+  (PIN == P37 ? (PP37) : \
 (0)))))))))))))))))
 
 // ===================================================================================
 // WRITE pin
 // ===================================================================================
 #define PIN_write(PIN, val) \
-  (PIN == P10 ? (P1_0 = val) : \
-  (PIN == P11 ? (P1_1 = val) : \
-  (PIN == P12 ? (P1_2 = val) : \
-  (PIN == P13 ? (P1_3 = val) : \
-  (PIN == P14 ? (P1_4 = val) : \
-  (PIN == P15 ? (P1_5 = val) : \
-  (PIN == P16 ? (P1_6 = val) : \
-  (PIN == P17 ? (P1_7 = val) : \
-  (PIN == P30 ? (P3_0 = val) : \
-  (PIN == P31 ? (P3_1 = val) : \
-  (PIN == P32 ? (P3_2 = val) : \
-  (PIN == P33 ? (P3_3 = val) : \
-  (PIN == P34 ? (P3_4 = val) : \
-  (PIN == P35 ? (P3_5 = val) : \
-  (PIN == P36 ? (P3_6 = val) : \
-  (PIN == P37 ? (P3_7 = val) : \
+  (PIN == P10 ? (PP10 = val) : \
+  (PIN == P11 ? (PP11 = val) : \
+  (PIN == P12 ? (PP12 = val) : \
+  (PIN == P13 ? (PP13 = val) : \
+  (PIN == P14 ? (PP14 = val) : \
+  (PIN == P15 ? (PP15 = val) : \
+  (PIN == P16 ? (PP16 = val) : \
+  (PIN == P17 ? (PP17 = val) : \
+  (PIN == P30 ? (PP30 = val) : \
+  (PIN == P31 ? (PP31 = val) : \
+  (PIN == P32 ? (PP32 = val) : \
+  (PIN == P33 ? (PP33 = val) : \
+  (PIN == P34 ? (PP34 = val) : \
+  (PIN == P35 ? (PP35 = val) : \
+  (PIN == P36 ? (PP36 = val) : \
+  (PIN == P37 ? (PP37 = val) : \
 (0)))))))))))))))))
+
+// ===================================================================================
+// Convert Pin for Inline Assembly and Direct Manipulation
+// ===================================================================================
+#define PIN_h_a(PIN) _P##PIN
+#define PIN_h_s(PIN) P##PIN
+
+#define PIN_asm(PIN) PIN_h_a(PIN)
+#define PIN_set(PIN) PIN_h_s(PIN)
 
 // ===================================================================================
 // (PORT, PIN) manipulation macros
 // ===================================================================================
-#define PORT_input(PORT, PIN)     (P##PORT##_DIR_PU &= ~(1<<PIN), P##PORT##_MOD_OC &= ~(1<<PIN))
-#define PORT_input_PU(PORT, PIN)  (P##PORT##_MOD_OC |=  (1<<PIN), P##PORT##_DIR_PU |=  (1<<PIN))
-#define PORT_output(PORT, PIN)    (P##PORT##_MOD_OC &= ~(1<<PIN), P##PORT##_DIR_PU |=  (1<<PIN))
-#define PORT_output_OD(PORT, PIN) (P##PORT##_MOD_OC |=  (1<<PIN), P##PORT##_DIR_PU &= ~(1<<PIN))
+#define PORT_h_i(PORT, PIN)       (P##PORT##_DIR_PU &= ~(1<<PIN), P##PORT##_MOD_OC &= ~(1<<PIN))
+#define PORT_h_iP(PORT, PIN)      (P##PORT##_MOD_OC |=  (1<<PIN), P##PORT##_DIR_PU |=  (1<<PIN))
+#define PORT_h_o(PORT, PIN)       (P##PORT##_MOD_OC &= ~(1<<PIN), P##PORT##_DIR_PU |=  (1<<PIN))
+#define PORT_h_oO(PORT, PIN)      (P##PORT##_MOD_OC |=  (1<<PIN), P##PORT##_DIR_PU &= ~(1<<PIN))
 
-#define PORT_low(PORT, PIN)       P##PORT##_##PIN = 0
-#define PORT_high(PORT, PIN)      P##PORT##_##PIN = 1
-#define PORT_toggle(PORT, PIN)    P##PORT##_##PIN = !P##PORT##_##PIN
-#define PORT_read(PORT, PIN)      (P##PORT##_##PIN)
-#define PORT_write(PORT,PIN,val)  P##PORT##_##PIN = val
+#define PORT_h_l(PORT, PIN)       PP##PORT##PIN = 0
+#define PORT_h_h(PORT, PIN)       PP##PORT##PIN = 1
+#define PORT_h_t(PORT, PIN)       PP##PORT##PIN = !PP##PORT##PIN
+#define PORT_h_r(PORT, PIN)       (PP##PORT##PIN)
+#define PORT_h_w(PORT,PIN,val)    PP##PORT##PIN = val
+
+#define PORT_input(PORT, PIN)     PORT_h_i(PORT, PIN)
+#define PORT_input_PU(PORT, PIN)  PORT_h_iP(PORT, PIN)
+#define PORT_output(PORT, PIN)    PORT_h_o(PORT, PIN)
+#define PORT_output_OD(PORT, PIN) PORT_h_oO(PORT, PIN)
+
+#define PORT_low(PORT, PIN)       PORT_h_l(PORT, PIN)
+#define PORT_high(PORT, PIN)      PORT_h_h(PORT, PIN)
+#define PORT_toggle(PORT, PIN)    PORT_h_t(PORT, PIN)
+#define PORT_read(PORT, PIN)      PORT_h_r(PORT, PIN)
+#define PORT_write(PORT,PIN,val)  PORT_h_w(PORT, PIN, val)
 
 // ===================================================================================
 // Start PWM on pin (must be a PWM-capable pin!!!)
