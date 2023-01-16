f41868488c0060de526f81424ff5ac6b4db51bca
wagiminator <wagiminator@web.de>
2023-01-16 20:05:57 +0100

    Added new sketch

--- CH55xE_DevStick/software/bootkey/bootkey.c
+++ CH55xE_DevStick/software/bootkey/bootkey.c
@@ -10,7 +10,7 @@
 //
 // Description:
 // ------------
-// Jump to bootloader on ACT-button press
+// Jump to bootloader on ACT-button press. Built-in LED lights up when in boot mode.
 //
 // References:
 // -----------
@@ -30,21 +30,24 @@
 // ===================================================================================
 // Libraries, Definitions and Macros
 // ===================================================================================
-#include <config.h>           // user configurations
-#include <ch554.h>            // CH55x header file
-#include <clock.h>            // system clock functions
-#include <gpio.h>             // GPIO functions
-#include <boot.h>             // for bootloader functions
+#include <config.h>             // user configurations
+#include <ch554.h>              // CH55x header file
+#include <clock.h>              // system clock functions
+#include <gpio.h>               // GPIO functions
+#include <boot.h>               // for bootloader functions
 
 // ===================================================================================
 // Main Function
 // ===================================================================================
 void main(void) {
-  CLK_config();               // configure system clock
-  PIN_input_PU(PIN_ACTKEY);   // set ACT key pin to INPUT PULLUP
+  CLK_config();                 // configure system clock
+  PIN_high(PIN_LED);            // turn off built-in LED
+  PIN_input_PU(PIN_ACTKEY);     // set ACT key pin to INPUT PULLUP
 
   while(1) {
-    if(!PIN_read(PIN_ACTKEY)) // ACT key pressed?
-      BOOT_now();             // jump to bootloader
+    if(!PIN_read(PIN_ACTKEY)) { // ACT key pressed?
+      PIN_low(PIN_LED);         // turn on built-in LED
+      BOOT_now();               // jump to bootloader
+    }
   }
 }
