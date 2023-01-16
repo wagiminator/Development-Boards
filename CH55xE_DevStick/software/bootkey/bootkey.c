b5aaa897c26040d58c69588bdf0b2b8292ad3a3d
wagiminator <wagiminator@web.de>
2023-01-16 19:57:29 +0100

    Added new sketch

---
+++ CH55xE_DevStick/software/bootkey/bootkey.c
@@ -0,0 +1,50 @@
+// ===================================================================================
+// Project:   Bootloader Demo for CH551, CH552, CH554
+// Version:   v1.0
+// Year:      2022
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
+//
+// Description:
+// ------------
+// Jump to bootloader on ACT-button press
+//
+// References:
+// -----------
+// - Blinkinlabs: https://github.com/Blinkinlabs/ch554_sdcc
+// - WCH Nanjing Qinheng Microelectronics: http://wch.cn
+//
+// Compilation Instructions:
+// -------------------------
+// - Chip:  CH551, CH552 or CH554
+// - Clock: doesn't matter
+// - Adjust the firmware parameters in include/config.h if necessary.
+// - Make sure SDCC toolchain and Python3 with PyUSB is installed.
+// - Press BOOT button on the board and keep it pressed while connecting it via USB
+//   with your PC.
+// - Run 'make flash'.
+
+// ===================================================================================
+// Libraries, Definitions and Macros
+// ===================================================================================
+#include <config.h>           // user configurations
+#include <ch554.h>            // CH55x header file
+#include <clock.h>            // system clock functions
+#include <gpio.h>             // GPIO functions
+#include <boot.h>             // for bootloader functions
+
+// ===================================================================================
+// Main Function
+// ===================================================================================
+void main(void) {
+  CLK_config();               // configure system clock
+  PIN_input_PU(PIN_ACTKEY);   // set ACT key pin to INPUT PULLUP
+
+  while(1) {
+    if(!PIN_read(PIN_ACTKEY)) // ACT key pressed?
+      BOOT_now();             // jump to bootloader
+  }
+}
---
+++ CH55xE_DevStick/software/bootkey/include/config.h
@@ -0,0 +1,29 @@
+// ===================================================================================
+// User Configurations for CH55xE Development Stick
+// ===================================================================================
+
+#pragma once
+
+// Pin definitions
+#define PIN_NEO             P14       // pin connected to NeoPixel
+#define PIN_LED             P15       // pin connected to LED
+#define PIN_TOUCHKEY        P16       // pin connected to touch key
+#define PIN_ACTKEY          P17       // pin connected to ACT-button
+#define LED_BUILTIN         P15       // builtin LED
+
+// NeoPixel configuration
+#define NEO_GRB                       // type of pixel: NEO_GRB or NEO_RGB
+
+// USB device descriptor
+#define USB_VENDOR_ID       0x6666    // VID: Prototype
+#define USB_PRODUCT_ID      0x6666    // PID: Prototype
+#define USB_DEVICE_VERSION  0x0100    // v1.0 (BCD-format)
+
+// USB configuration descriptor
+#define USB_MAX_POWER_mA    50        // max power in mA 
+
+// USB descriptor strings
+#define MANUFACTURER_STR    'w','a','g','i','m','i','n','a','t','o','r'
+#define PRODUCT_STR         'C','H','5','5','x','E',' ','D','e','v','S','t','i','c','k'
+#define SERIAL_STR          'C','D','C'
+#define INTERFACE_STR       'C','D','C','-','S','e','r','i','a','l'
