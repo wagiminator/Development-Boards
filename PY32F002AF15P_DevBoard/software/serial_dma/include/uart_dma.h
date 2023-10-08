47e43138ddc5c30a87d81a8ebadb6d93ac09e385
wagiminator <wagiminator@web.de>
2023-10-08 19:13:57 +0200

    Some updates

---
+++ PY32F002AF15P_DevBoard/software/serial_dma/include/uart_dma.h
@@ -0,0 +1,119 @@
+// ===================================================================================
+// UART1 with DMA RX Buffer for PY32F0xx                                      * v1.0 *
+// ===================================================================================
+//
+// Functions available:
+// --------------------
+// UART_init()              Init UART with 8N1 and default BAUD rate (115200)
+// UART_setBaud(n)          Set BAUD rate
+// UART_setDataBits(n)      Set number of data bits (n = 8, 9)
+// UART_setStopBits(n)      Set number of stop bits (n = 1, 2)
+// UART_setNoParity()       Set no parity bit
+// UART_setOddParity()      Set parity bit, odd
+// UART_setEvenParity()     Set parity bit, even
+//
+// UART_ready()             Check if UART is ready to write
+// UART_available()         Check if there is something to read
+// UART_completed()         Check if transmission is completed
+//
+// UART_read()              Read character via UART
+// UART_write(c)            Send character via UART
+//
+// UART_enable()            Enable USART
+// UART_disable()           Disable USART
+// UART_TX_enable()         Enable transmitter
+// UART_TX_disable()        Disable transmitter
+// UART_RX_enable()         Enable receiver
+// UART_RX_disable()        Disable receiver
+//
+// If print functions are activated (see below, print.h must be included):
+// UART_printD(n)           Print decimal value
+// UART_printW(n)           Print 32-bit hex word value
+// UART_printH(n)           Print 16-bit hex half-word value
+// UART_printB(n)           Print  8-bit hex byte value
+// UART_printS(s)           Print string
+// UART_print(s)            Print string (alias)
+// UART_println(s)          Print string with newline
+// UART_newline()           Send newline
+//
+// USART1 pin mapping (set below in UART parameters):
+// --------------------------------------------------
+// UART_MAP   TX-pin  RX-pin
+//       0     PA2     PA3
+//       1     PA7     PA8
+//       2     PA9     PA10
+//       3     PA14    PA13
+//       4     PA14    PA15
+//       5     PB6     PB7
+//       6     PF1     PF0
+//       7     no pin mapping
+//
+// 2023 by Stefan Wagner:   https://github.com/wagiminator
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "py32f0xx.h"
+
+// UART parameters
+#define UART_BAUD             115200    // default UART baud rate
+#define UART_RX_BUF_SIZE      64        // UART RX buffer size
+#define UART_MAP              0         // UART pin mapping (see above)
+#define UART_PRINT            0         // 1 = include print functions (needs print.h)
+#define DMA_CHANNEL           1         // define DMA channel (1 - 3)
+
+// UART macros
+#define UART_ready()          (USART1->SR & USART_SR_TXE)         // ready to write
+#define UART_completed()      (USART1->SR & USART_SR_TC)          // transmission completed
+
+#define UART_enable()         USART1->CR1 |= USART_CR1_UE         // enable USART
+#define UART_disable()        USART1->CR1 &= ~USART_CR1_UE        // disable USART
+#define UART_TX_enable()      USART1->CR1 |= USART_CR1_TE         // enable transmitter
+#define UART_TX_disable()     USART1->CR1 &= ~USART_CR1_TE        // disable transmitter
+#define UART_RX_enable()      USART1->CR1 |= USART_CR1_RE         // enable receiver
+#define UART_RX_disable()     USART1->CR1 &= ~USART_CR1_RE        // disable receiver
+
+#define UART_setBAUD(n)       USART1->BRR = ((2*F_CPU/(n))+1)/2   // set BAUD rate
+#define UART_setDataBits(n)   (n==9 ? (USART1->CR1 |= USART_CR1_M) : (USART1->CR1 &= ~USART_CR1_M))
+#define UART_setStopBits(n)   (n==2 ? (USART1->CR2 |= USART_CR2_STOP) : (USART1->CR2 &= ~USART_CR2_STOP))
+#define UART_setEvenParity()  {USART1->CR1 |= USART_CR1_PCE; USART1->CR1 &= ~USART_CR1_PS;}
+#define UART_setOddParity()   {USART1->CR1 |= USART_CR1_PCE; USART1->CR1 |=  USART_CR1_PS;}
+#define UART_setNoParity()    USART1->CR1 &= ~USART_CR1_PCE
+
+// UART functions
+void UART_init(void);                             // init UART with default BAUD rate
+char UART_read(void);                             // read character via UART
+void UART_write(const char c);                    // send character via UART
+uint8_t UART_available(void);                     // check if there is something to read
+
+// Additional print functions (if activated, see above)
+#if UART_PRINT == 1
+#include "print.h"
+#define UART_printD(n)    printD(n, UART_write)   // print decimal as string
+#define UART_printW(n)    printW(n, UART_write)   // print word as string
+#define UART_printH(n)    printH(n, UART_write)   // print half-word as string
+#define UART_printB(n)    printB(n, UART_write)   // print byte as string
+#define UART_printS(s)    printS(n, UART_write)   // print string
+#define UART_println(s)   println(s, UART_write)  // print string with newline
+#define UART_print        UART_printS             // alias
+#define UART_newline()    UART_write('\n')        // send newline
+#endif
+
+// DMA channel defines
+#if   DMA_CHANNEL == 1
+  #define DMA_CHAN        DMA1_Channel1
+  #define DMA_POS         SYSCFG_CFGR3_DMA1_MAP_Pos
+#elif DMA_CHANNEL == 2
+  #define DMA_CHAN        DMA1_Channel2
+  #define DMA_POS         SYSCFG_CFGR3_DMA2_MAP_Pos
+#elif DMA_CHANNEL == 3
+  #define DMA_CHAN        DMA1_Channel3
+  #define DMA_POS         SYSCFG_CFGR3_DMA3_MAP_Pos
+#endif
+
+#ifdef __cplusplus
+};
+#endif
---
+++ PY32F002AF15P_DevBoard/software/serial_dma/serial_dma.c
@@ -0,0 +1,43 @@
+// ===================================================================================
+// Project:   Example for PY32F002A
+// Version:   v1.0
+// Year:      2023
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
+//
+// Description:
+// ------------
+// Echoes data sent via UART. Uses DMA for RX, which should not exist on PY32F002A.
+//
+// Compilation Instructions:
+// -------------------------
+// - Make sure GCC toolchain (gcc-arm-none-eabi) and Python3 with PySerial is
+//   installed. If necessary, a driver for the USB-to-serial converter used must
+//   be installed.
+// - Connect your MCU board via USB to your PC.
+// - Set the MCU to boot mode by holding down the BOOT key and then pressing and 
+//   releasing the RESET key. Finally release the BOOT key.
+// - Run 'make flash'.
+
+
+// ===================================================================================
+// Libraries, Definitions and Macros
+// ===================================================================================
+#include "system.h"           // system functions
+#include "uart_dma.h"         // UART with DMA functions
+
+// ===================================================================================
+// Main Function
+// ===================================================================================
+int main (void) {
+  // Setup
+  UART_init();                // init UART (TX: PA2, RX: PA3, BAUD: 115200, 8N1)
+  
+  // Loop
+  while(1) {
+    UART_write(UART_read());  // echo any received character
+  }
+}
