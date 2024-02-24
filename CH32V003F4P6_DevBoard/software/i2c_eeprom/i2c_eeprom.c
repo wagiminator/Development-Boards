862943548968f80ae9032b3d1958c8de19c21748
wagiminator <wagiminator@web.de>
2024-02-24 17:56:31 +0100

    Some updates

---
+++ CH32V003F4P6_DevBoard/software/i2c_eeprom/i2c_eeprom.c
@@ -0,0 +1,60 @@
+// ===================================================================================
+// Project:   Example for CH32V003
+// Version:   v1.0
+// Year:      2023
+// Author:    Stefan Wagner
+// Github:    https://github.com/wagiminator
+// EasyEDA:   https://easyeda.com/wagiminator
+// License:   http://creativecommons.org/licenses/by-sa/3.0/
+// ===================================================================================
+//
+// Description:
+// ------------
+// Transfers content of 24Cxx I2C EEPROM via UART.
+//
+// References:
+// -----------
+// - CNLohr ch32v003fun: https://github.com/cnlohr/ch32v003fun
+// - WCH Nanjing Qinheng Microelectronics: http://wch.cn
+//
+// Compilation Instructions:
+// -------------------------
+// - Make sure GCC toolchain (gcc-riscv64-unknown-elf, newlib) and Python3 with PyUSB
+//   is installed. In addition, Linux requires access rights to WCH-LinkE programmer.
+// - Connect the WCH-LinkE programmer to the MCU board.
+// - Run 'make flash'.
+
+
+// ===================================================================================
+// Libraries, Definitions and Macros
+// ===================================================================================
+#include <system.h>                               // system functions
+#include <debug_serial.h>                         // serial debug functions
+#include <i2c_soft.h>                             // software I2C functions
+#include <eeprom_24c.h>                           // 24Cxx I2C EEPROM functions
+
+// ===================================================================================
+// Main Function
+// ===================================================================================
+int main(void) {
+  // Setup
+  DEBUG_init();                                   // init serial debug
+  I2C_init();                                     // init I2C
+  EEPROM_update(0, 0x55);                         // write value to address 0
+  
+  // Loop
+  while(1) {
+    uint8_t i,j;
+    uint8_t addr = 0;
+    DEBUG_println("EEPROM Hex Dump:");
+    for(j=16; j; j--) {
+      DEBUG_printH(addr); DEBUG_print(": ");
+      for(i=16; i; i--) {
+        DEBUG_printB(EEPROM_read(addr++)); DEBUG_write(' ');
+      }
+      DEBUG_println("");
+    }
+    DEBUG_println("");
+    DLY_ms(1000);                                 // wait a second
+  }
+}
---
+++ CH32V003F4P6_DevBoard/software/i2c_eeprom/include/eeprom_24c.h
@@ -0,0 +1,39 @@
+// ===================================================================================
+// 24Cxx I2C EEPROM Functions                                                 * v1.0 *
+// ===================================================================================
+//
+// Simple 24Cxx I2C EEPROM functions.
+//
+// Functions available:
+// --------------------
+// EEPROM_read(a)             read single byte from EEPROM address (a)
+// EEPROM_write(a,v)          write single byte (v) to EEPROM address (a)
+// EEPROM_update(a,v)         write if changed (reduces write cycles)
+// EEPROM_writeStream(a,p,l)  write (l) number of bytes starting at address (a) from pointer (p)
+// EEPROM_readStream(a,p,l)   read (l) number of bytes starting at address (a) to pointer (p)
+//
+// 2024 by Stefan Wagner:     https://github.com/wagiminator
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "i2c_soft.h"
+#include "system.h"
+
+// EEPROM definitions
+#define EEPROM_ADDR       0xa0      // EEPROM I2C device address including write bit
+#define EEPROM_PAGESIZE   8         // page size of EEPROM
+
+// EEPROM functions
+uint8_t EEPROM_read(uint16_t addr);                 // read single byte from EEPROM
+void EEPROM_write(uint16_t addr, uint8_t value);    // write single byte to EEPROM
+void EEPROM_update(uint16_t addr, uint8_t value);   // write if changed (reduces write cycles)
+void EEPROM_writeStream(uint16_t addr, uint8_t* ptr, uint8_t len);
+void EEPROM_readStream(uint16_t addr, uint8_t* ptr, uint8_t len);
+
+#ifdef __cplusplus
+};
+#endif
